1f1f99498bfa5a9b7aaf1c278db4cc865fa859f4
Elliot Yap <yilliot@gmail.com>
2018-03-13 11:18:34 +0800

    manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode : 'development',
   entry: {
     'app' : './src/index.js',
-    'jquery' : 'jquery'
+    'vendor' : ['jquery', 'lodash', 'pixi.js']
   },
   plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename : 'index.html',
-      title : 'Output Management'
+      filename : 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/cms/zone-list.html',
+      filename : 'cms/zone-list.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/directory/index.html',
+      filename : 'directory/index.html'
     })
   ],
   output: {
